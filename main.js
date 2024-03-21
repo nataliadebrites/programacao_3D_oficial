4f0d7ae4fd351c8c5d295a7a66c991405c33cb92
Natália de Brites B <113113412+nataliadebrites@users.noreply.github.com>
2024-03-21 14:30:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const botoes=document.querySelectorAll(".botao");
+const botoes= document.querySelectorAll(".botao");
+const textos= document.querySelectorAll(".aba-conteudo");
+const contadores =document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2024-03-22T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 console.log(botoes);
 
 for (let i=0; i<botoes.length; i++){
@@ -6,8 +11,10 @@ for (let i=0; i<botoes.length; i++){
         
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 
 }
